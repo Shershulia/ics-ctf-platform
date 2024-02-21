12c89c6eff53df25566a47a80dc68b2a561b882e
Shershulia <krol31072002@gmail.com>
2024-02-21 15:23:42 +0100

    Popup center start filling it up

--- components/ProblemPopUp/PopUp.tsx
+++ components/ProblemPopUp/PopUp.tsx
@@ -8,11 +8,10 @@ const PopUp = ({ children, trigger, setTrigger } : PopUpProps) => {
     return (
         trigger &&
             (
-                <div className={"max-h-screen"}>
-                    <div className={"fixed top-0 left-0 w-full h-screen bg-black border-darkGray z-40 opacity-30"} onClick={setTrigger}>
-                    </div>
-                    <div className={"fixed p-12 w-full h-full m-auto top-[20%] max-w-[640px] max-h-[640px] bg-white opacity-100 z-50"}>
-                        <button onClick={setTrigger}> Close</button>
+                <div className="fixed inset-0 flex justify-center items-center z-50">
+                    <div className="absolute inset-0 bg-black opacity-30" onClick={setTrigger}></div>
+                    <div className="relative p-12 w-[640px] h-[640px] bg-white">
+                        <button className="absolute top-0 right-0 m-4" onClick={setTrigger}>Close</button>
                         {children}
                     </div>
                 </div>
