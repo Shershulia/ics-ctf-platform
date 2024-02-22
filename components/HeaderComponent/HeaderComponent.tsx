595b25621abc35134e166458057c2a8a6284b0b0
Shershulia <krol31072002@gmail.com>
2024-02-22 16:09:34 +0100

    Navigation bar fix

--- components/HeaderComponent/HeaderComponent.tsx
+++ components/HeaderComponent/HeaderComponent.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {MyAccountDropDown} from "@/components";
+import Link from "next/link";
 
 const HeaderComponent = () => {
     return (
         <div className={"w-full bg-black py-4"}>
             <div className={"w-[70%] m-auto flex justify-between items-center"}>
-                <p className={"text-white font-bold"}>CTF ICS/OT</p>
+                <Link className={"cursor-pointer"} href={"/"}>
+                    <p className={"text-white font-bold"}>CTF ICS/OT</p>
+                </Link>
                 <MyAccountDropDown/>
             </div>
 
