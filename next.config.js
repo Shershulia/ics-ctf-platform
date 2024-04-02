e35ce297f3425b69c86ef3b86faefa35573c4210
Shershulia <krol31072002@gmail.com>
2024-04-02 17:01:41 +0200

    Fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.stats = {
+      warningsFilter: [/Page Without Valid React Component/],
+    };
+
+    return config;
+  },
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'html'],
   swcMinify: true,
