a85b086dd4f0a54897161cea762d59f42b923496
Shershulia <krol31072002@gmail.com>
2024-04-02 16:29:12 +0200

    Fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'html'],
   swcMinify: true,
   async rewrites () {
     return [
