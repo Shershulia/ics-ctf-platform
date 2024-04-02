ed3ba893721b6f10c3fbbe26decea363d423e8fc
Shershulia <krol31072002@gmail.com>
2024-04-02 14:11:45 +0200

    XSS-problem

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  async rewrites () {
+    return [
+      {
+        source: '/problems/xss-problem',
+        destination: '/html/xss-problem/xss-problem.html'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
