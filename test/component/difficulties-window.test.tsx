3dedb9170b7988f72488c8f036d90102e3b8e27a
Shershulia <krol31072002@gmail.com>
2024-04-15 21:08:57 +0200

    Tests for difficulties components

--- test/component/difficulties-window.test.tsx
+++ test/component/difficulties-window.test.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import axios from 'axios';
 import {describe, expect, it} from "@jest/globals";
 import DifficultyLevelWindow from "../../components/MainPage/FiltersRight/DifficultyLevelWindow";
+import '@testing-library/jest-dom/extend-expect';
 
 
 describe('DifficultyLevelWindow Component', () => {
-        render(<DifficultyLevelWindow />);
 
         it('renders title correctly', async () => {
+            render(<DifficultyLevelWindow />);
             expect(screen.getByText('Difficulties')).toBeDefined();
     });
-
-
-
-
+    it('renders difficulty tabs with aria-label correctly', async () => {
+        render(<DifficultyLevelWindow />);
+        const difficultyTab = screen.getByRole('tab');
+        // @ts-ignore
+        expect(difficultyTab).toHaveAttribute('aria-selected', 'true');
+    });
+    it('renders difficulty tabs initial text', async () => {
+        render(<DifficultyLevelWindow />);
+        const difficultyTab = screen.getByRole('tab');
+        // @ts-ignore
+        expect(difficultyTab).toHaveTextContent('All');
+    });
 });
